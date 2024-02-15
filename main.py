7ece0051252ffe5ca797ae84ceb4e6b3330eabf1
jeffrey <vkefhdl1@gmail.com>
2024-02-15 20:15:11 +0000

    change project_dir exist check to False

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ data_path = os.path.join(root_path, 'data')
 @click.option('--config', type=click.Path(exists=True))
 @click.option('--qa_data_path', type=click.Path(exists=True), default=os.path.join(data_path, 'qa.parquet'))
 @click.option('--corpus_data_path', type=click.Path(exists=True), default=os.path.join(data_path, 'corpus.parquet'))
-@click.option('--project_dir', type=click.Path(exists=True), default=os.path.join(root_path, 'benchmark'))
+@click.option('--project_dir', type=click.Path(exists=False), default=os.path.join(root_path, 'benchmark'))
 def main(config, qa_data_path, corpus_data_path, project_dir):
     load_dotenv()
     if not os.path.exists(project_dir):
